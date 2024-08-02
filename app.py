1d17f8d42285dc40cfd7b01c93741d766f9a244c
Anish Katta <kattaa1@uci.edu>
2024-08-01 21:05:13 -0700

    created form for course codes

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return render_template('classes.html')
+
 
 
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000,debug=True)
+
